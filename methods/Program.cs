867b74c67bb17dd6205795e0c76964e379e3bfb7
Pablo Colturi <pcolturi@fastmail.com>
2024-10-16 12:34:39 +0200

    methods exercise

---
+++ methods/Program.cs
@@ -0,0 +1,44 @@
+﻿
+class Program {
+    static void Main() {
+
+        Methods.printTripleNumberConsole();
+    }
+
+    class Methods {
+
+        private static int _NumberParsed;
+
+        private static bool askForNumber() {
+
+            Console.WriteLine("Enter a number:");
+            var input = Console.ReadLine();
+
+            if (!string.IsNullOrEmpty(input) && int.TryParse(input, out _)) {
+                _NumberParsed = int.Parse(input);
+                return true;
+            } else {
+                Console.WriteLine("Please enter a number.");
+                return false;
+            }
+        }
+
+
+        private static void printTripleNumber() {
+            
+            var triple = _NumberParsed * 3;
+            Console.WriteLine(triple);
+        }
+
+        public static void printTripleNumberConsole() {
+
+            Console.WriteLine("# 1: Print triple of a number from console input.");
+            while (!askForNumber()) {
+                askForNumber();
+                break;
+            }
+            printTripleNumber();
+        }
+
+    }
+}
