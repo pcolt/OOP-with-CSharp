9305476ca46d20282fa91ff72ae1ff395094b721
Pablo Colturi <pcolturi@fastmail.com>
2024-10-18 09:46:12 +0200

    add isValid logic

--- 05-oop/People.cs
+++ 05-oop/People.cs
@@ -6,12 +6,20 @@ class Inspector
 {
     public void StampTickets(List<ITicket> tickets) {
         foreach (var ticket in tickets) {
+            if (ticket.IsValid == false) {
+                Console.WriteLine("Oh no! This ticket has no rides left!");
+                continue;
+            }
             ticket.Stamp();
         }
     }
     
     public void StampTicketsInBus(Bus bus) {
         foreach (var person in bus.Travellers) {
+            if (person.Ticket.IsValid == false) {
+                Console.WriteLine($"Oh no! {person.Name} has no rides left!");
+                continue;
+            }
             person.Ticket.Stamp();
         }
     }
--- 05-oop/Tickets.cs
+++ 05-oop/Tickets.cs
@@ -3,17 +3,17 @@ namespace Tickets;
 interface ITicket
 {
     int RidesLeft { get; }
+    bool IsValid { get; }
     void Stamp();
 }
 
 class SingleTicket : ITicket
 {
     public int RidesLeft { get; private set; } = 1;
-
+    public bool IsValid { get { return RidesLeft > 0; } }
     public void Stamp() {
         if (RidesLeft == 0) {
-            Console.WriteLine("Oh no! Ticket has no rides left!");
-            return;
+            throw new Exception("Ticket has no rides left!");
         }
         RidesLeft -= 1;
     }
@@ -22,11 +22,11 @@ class SingleTicket : ITicket
 class TicketBooklet : ITicket
 {
     public int RidesLeft { get; private set; } = 10;
+    public bool IsValid { get { return RidesLeft > 0; } }
 
     public void Stamp() {
         if (RidesLeft == 0) {
-            Console.WriteLine("Oh no! Ticket has no rides left!");
-            return;
+            throw new Exception("Ticket has no rides left!");
         }
         RidesLeft -= 1;
     }
