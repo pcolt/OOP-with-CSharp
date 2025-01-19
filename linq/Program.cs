d8cdabbffc442603e0abee452a299b2fe1b7faf5
Pablo Colturi <pablo.colturi@kappas.it>
2025-01-19 13:26:22 +0100

    change tests folder name + add linq wip

---
+++ linq/Program.cs
@@ -0,0 +1,32 @@
+﻿class Program {
+    static void Main() {
+        var strings1 = new [] {
+            "1",
+            "",
+            "pippo",
+            "2",
+            "4",
+            "17",
+            "nope",
+            "21",
+        };
+
+        var result = strings1
+            .Where(s => s.Length > 0 && int.TryParse(s, out _))
+            .Select(int.Parse)
+            .Select(i => i * 3)
+            .GroupBy(i => i % 2 == 0)
+            .Select(g => new {
+                IsEven = g.Key,
+                numbers = g.ToList()
+                });
+                
+        
+        foreach (var item in result) {
+            Console.WriteLine(item);
+            foreach (var number in item.numbers) {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
